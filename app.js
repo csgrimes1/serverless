a33a29b45e7ea03deebaa7dbd67d09ea98f17006
Chris Grimes <christopher.grimes@adidas.com>
2017-12-14 15:09:06 -0800

    First commit.

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ var app = express();
 
 app.get('/', function(req, res) {
   res.send({
-    "Output": "Hello World!"
+    "Output": "Hello World! GET"
   });
 });
 
 app.post('/', function(req, res) {
   res.send({
-    "Output": "Hello World!"
+    "Output": "Hello World! POST"
   });
 });
 
